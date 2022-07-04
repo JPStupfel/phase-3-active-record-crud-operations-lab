6782a702b8182160a3cd24212c219abfccd97278
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-04 14:24:45 -0500

    commit tree 089fb7920ee9702c77b49ecea48c43580185cbd0

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,41 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title title
+        self.create(title: title)
+    end
+
+    def self.first_movie
+        self.first
+    end
+    def self.last_movie
+        self.last
+    end
+    def self.movie_count
+        self.count
+    end
+    def self.find_movie_with_id id
+        self.find(id)
+    end
+    def self.find_movie_with_attributes attributes
+        self.find_by(attributes)
+    end
+    def self.find_movies_after_2002
+        self.where("release_date >2002")
+    end
+    def update_with_attributes attributes
+        self.update attributes
+    end
+    def self.update_all_titles attributes
+        self.update title: attributes
+    end
+    def self.delete_by_id id
+        x = find_movie_with_id id
+        x.destroy
+
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
+        
+    end
 
 end
